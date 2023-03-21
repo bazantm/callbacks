e1a01125da22aee2c7fdf2c2cecc7dae0a4652cb
Michael Bazant <bazantm@gmail.com>
2023-03-21 17:54:55 +0100

    Initial commit

---
+++ test_callback.py
@@ -0,0 +1,17 @@
+import requests
+
+def make_request(url, callback):
+    response = requests.get(url)
+    callback(response)
+
+
+def handle_response(response):
+    if response.status_code == 200:
+        data = response.json()
+        print('Data received: ', data)
+    else:
+        print('Error: ', response.status_code)
+
+
+if __name__ == '__main__':
+    make_request('http://api.open-notify.org/astros.json', handle_response)
